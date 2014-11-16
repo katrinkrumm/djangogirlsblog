a3913ec78dd179d93bcb315f3dec22176ffdf140
anonymous <info@katrinkrumm.de>
2014-11-16 15:56:27 +0100

    changes

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
+    posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-    return render(request, 'blog/post_list.html', {})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
